eca9f9ac6373597be40c38e5adf204ea301966d8
Claudia Zeledon <cjzeledon@gmail.com>
2018-07-27 16:14:24 -0400

    First Commit

---
+++ src/main/java/com/claudiazeledon/livetreeview/LivetreeviewApplication.java
@@ -0,0 +1,12 @@
+package com.claudiazeledon.livetreeview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivetreeviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivetreeviewApplication.class, args);
+	}
+}
